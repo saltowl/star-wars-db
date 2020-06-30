61d640f8342bb30a7fba7e670c186f0350cce51b
saltowl <vladislava-shakirova@mail.ru>
2020-06-30 15:07:58 +0300

    Updates

---
+++ precache-manifest.7f2675bf1b14d0aa0e533269ac6d7c4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83312888d637b09ef9b7bb425365f56",
+    "url": "/star-wars-db/index.html"
+  },
+  {
+    "revision": "27b2eaceeff92feb0660",
+    "url": "/star-wars-db/static/css/main.3f2cb7cd.chunk.css"
+  },
+  {
+    "revision": "38c775a1f2002169b5e5",
+    "url": "/star-wars-db/static/js/2.17af6cd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars-db/static/js/2.17af6cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b2eaceeff92feb0660",
+    "url": "/star-wars-db/static/js/main.5f391aa5.chunk.js"
+  },
+  {
+    "revision": "a60a5c9fea3db66514d7",
+    "url": "/star-wars-db/static/js/runtime-main.468c23db.js"
+  },
+  {
+    "revision": "d835081dfbc27013c33aaca072a46f81",
+    "url": "/star-wars-db/static/media/jedi-order-brands.d835081d.svg"
+  }
+]);
